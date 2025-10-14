789e6f344a6a69645e53bb9817ca1f6e92e8c9e6
ashwin071203 <164607519+ashwin071203@users.noreply.github.com>
2025-10-14 23:32:44 +0530

    first commit

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import '../App.css';
 
 export const Header = () => {
   const { pathname } = useLocation();
